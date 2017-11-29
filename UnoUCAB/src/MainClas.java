c770bfbfe19fa7421016c9f009eb61e5c40236e6
Alexander Garcia <alexdgn213@gmail.com>
2017-11-28 21:12:31 -0400

    Primera prueba de transmicion

--- UnoUCAB/src/MainClas.java
+++ UnoUCAB/src/MainClas.java
@@ -1,3 +1,6 @@
+
+import Comunicacion.ServicioTransmicion;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,6 +17,7 @@ public class MainClas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ServicioTransmicion servicioTransmicion=new ServicioTransmicion();
         // TODO code application logic here
     }
     
